8e154477297b55bdfb04166f6d6ab9c7a0851512
Kalikost <ernestsg96@gmail.com>
2026-07-15 20:31:07 +0000

    Update dev_info.sh

--- bash/devices/dev_info.sh
+++ bash/devices/dev_info.sh
@@ -8,10 +8,10 @@ for device in "${devices[@]}"; do
 done
 
 #check how null destroys everything that goes into it
-echo "This sentence will dissapear into /dev/null" 
+echo "This sentence will disappear into /dev/null" 
 sleep 1
-echo "This sentence will disappear into the /dev/null folder" > /dev/null
-echo "Let's check it. Running a list of all files in the folder ..."
+echo "This sentence will disappear into the /dev/null" > /dev/null
+echo "Let's check it. Running a list of all files inside ..."
 ls -la /dev/null
 
 #Make some random numbers
